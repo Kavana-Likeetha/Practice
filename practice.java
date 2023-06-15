5d7b970d9acb04e598d4d6e22cfd1785d3e139b5
Kavana-Likeetha <CommonKL1224@gmail.com>
2023-06-15 08:07:51 +0530

    fourth commit

--- practice.java
+++ practice.java
@@ -2,6 +2,6 @@ public class practice {
     public static void main(String[] args) {
         int a = 10;
         int b = 10;
-        System.out.println((a + b) + "sum");
+        System.out.println((a * b) + "sum");
     }
 }
